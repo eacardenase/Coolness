adc042877e9ab0006c3d8949e6b7bc4602344b19
Edwin Cardenas <eacardenase@gmail.com>
2024-12-29 13:37:59 -0500

    feat: add loops

--- Coolness/main.c
+++ Coolness/main.c
@@ -8,7 +8,52 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//    for (int i = 0; i < 11; i++) {
+//        printf("%d. Edwin is Cool\n", i);
+//    }
+    
+//    int i = 0;
+//    
+//    while (i < 12) {
+//        printf("%d. Edwin is Cool\n", i);
+//        i++;
+//    }
+    
+//    int i;
+//
+//    for(i = 0; i < 12; i++) {
+//        printf("Checking i = %d\n", i);
+//        
+//        if (i + 90 == i * i) {
+//            break;
+//        }
+//    }
+//    
+//    printf("The answer is %d.\n", i);
+    
+//    int i;
+//    
+//    for (i = 0; i < 12; i++) {
+//        if (i % 3 == 0) {
+//            continue;
+//        }
+//        
+//        printf("Checking i = %d\n", i);
+//        
+//        if (i + 90 == i * i) {
+//            break;
+//        }
+//    }
+//    
+//    printf("The answer is %d.\n", i);
+    
+    int i = 0;
+    
+    do {
+        printf("%d. Edwin is Cool\n", i);
+        
+        i++;
+    } while (i < 12);
+    
     return 0;
 }
